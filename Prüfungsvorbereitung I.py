7e9d9191119740d807901281c1e0cab45f507892
hahahahallo <96128700+hahahahallo@users.noreply.github.com>
2021-12-14 15:50:20 +0100

    Add files via upload

---
+++ Prüfungsvorbereitung I.py
@@ -0,0 +1,7 @@
+#Dieses Programm aufgrund deines Gewichts und deiner Grösse den BMI aus.
+
+kg=input("Gib dein Gewicht in Kilogramm ein:")# kg ist die Eingabe des Gewichts
+m=input("Gib deine Grösse in Meter ein:")# m ist die Eingabe der Grösse
+
+bmi=int(kg)/(float(m)*float(m))# Die Werte werden verrechnet("BMI-Formel", Das Gewicht durch Grösse mal Grösse 
+print("BMI="+str(bmi))# Der BMI wird angezeigt/ausgegeben
